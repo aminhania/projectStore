8e0911e87f89280735232fc859b51deaf32a2f42
amineaouini <amineaouini565@gmail.com>
2019-11-04 16:47:33 +0100

    second

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-// var beats = 250;
-// var laptop1 = 1050 ;
-// var JBL = 240;
-// var smartWatch = 400;
-// var iphone = 1100;
-// var laptop2 = 2500;
+var beats = 250;
+var laptop1 = 1050 ;
+var JBL = 240;
+var smartWatch = 400;
+var iphone = 1100;
+var laptop2 = 2500;
 var sum = 0;
-function choos(obj){
+//function choos(obj){
 $('#btn1').on('click', function() {
-	sum = sum + $('#pr1').text();
-	$("#ttl").html('<span>' + sum + '</span>');
+	sum = sum + beats;
+	$("#ttl").html(sum);
 
 });
 
-}
+$('#btn2').on('click', function() {
+	sum = sum + laptop1;
+	$("#ttl").html(sum);
+
+});
+
+$('#btn3').on('click', function() {
+	sum = sum + JBL;
+	$("#ttl").html(sum);
+
+});
+
+$('#btn4').on('click', function() {
+	sum = sum + smartWatch;
+	$("#ttl").html(sum);
+
+});
+
+$('#btn1').on('click', function() {
+	sum = sum + beats;
+	$("#ttl").html(sum);
+
+});
+
