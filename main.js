f1143158b8f25f979e1f53b3bcce8aee752b8a71
amineaouini <amineaouini565@gmail.com>
2019-11-05 10:27:21 +0100

    third push

--- main.js
+++ main.js
@@ -6,33 +6,51 @@ var iphone = 1100;
 var laptop2 = 2500;
 var sum = 0;
 //function choos(obj){
+$('#pr1').html(beats);
+$("#pr2").html(laptop1);
+$("#pr3").html(JBL);
+$("#pr4").html(smartWatch);
+$("#pr5").html(iphone);
+$("#pr6").html(laptop2);
+
 $('#btn1').on('click', function() {
 	sum = sum + beats;
-	$("#ttl").html(sum);
+	$('#list').append("beats earphones<br>price: " + beats +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
 
 });
 
 $('#btn2').on('click', function() {
 	sum = sum + laptop1;
-	$("#ttl").html(sum);
+	$('#list').append("laptop1<br>price: " + laptop1 +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
 
 });
 
 $('#btn3').on('click', function() {
 	sum = sum + JBL;
-	$("#ttl").html(sum);
+	$('#list').append("JBL<br>price: " + JBL +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
 
 });
 
 $('#btn4').on('click', function() {
 	sum = sum + smartWatch;
-	$("#ttl").html(sum);
+	$('#list').append("smartWatch<br>price: " + smartWatch +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
 
 });
 
-$('#btn1').on('click', function() {
-	sum = sum + beats;
-	$("#ttl").html(sum);
+$('#btn5').on('click', function() {
+	sum = sum + iphone;
+	$('#list').append("iphone XI<br>price: " + iphone +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
 
 });
 
+$('#btn6').on('click', function() {
+	sum = sum + laptop2;
+	$('#list').append("laptop2<br>price: " + laptop2 +"<hr>")
+	$("#ttl").html('your total is: ' + sum);
+
+});
